ccfbefd5d1e48d30181b6bb6dad86cef2ad32be4
Alessandro <ajatoba@gmail.com>
2015-03-16 15:08:31 -0400

    Signed-off-by: Alessandro <ajatoba@gmail.com>

---
+++ EJMAndroid/src/br/com/devmedia/gestaoacademica/model/Docente.java
@@ -0,0 +1,32 @@
+package br.com.devmedia.gestaoacademica.model;
+
+public class Docente {
+
+    private String nome;
+    private String matricula;
+    private String titulacao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTitulacao() {
+        return titulacao;
+    }
+
+    public void setTitulacao(String titulacao) {
+        this.titulacao = titulacao;
+    }
+}
